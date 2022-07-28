9d6864dd517ae3d93f65be20a751420269b991dc
lefi <leofiloche.wk@gmail.com>
2022-07-28 09:56:25 +0200

    [DEPENDANCES] ajout du fichier requirements qui liste toutes les dépendences du projet et qui permet de toutes les installer en une commande

--- detectionQCM/opencvpython/systeme.py
+++ detectionQCM/opencvpython/systeme.py
@@ -1,16 +1,7 @@
 import os
-import ghostscript
 from pdf2image import convert_from_path
 
 
-def pdf2jpeg(pdf_input_path, jpeg_output_path):
-    args = ["pdf2jpeg", # actual value doesn't matter
-            "-dNOPAUSE",
-            "-sDEVICE=jpeg",
-            "-r144",
-            "-sOutputFile=" + jpeg_output_path,
-            pdf_input_path]
-    ghostscript.Ghostscript(*args)
 
 def pdf2png(pdf_input_path, png_output_path):
     pages = convert_from_path(pdf_input_path, 500)
