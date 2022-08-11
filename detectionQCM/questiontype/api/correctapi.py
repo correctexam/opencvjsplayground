ad8a70b78ae61b5c5d560d60d052eba71d92408d
lefi <leofiloche.wk@gmail.com>
2022-08-11 11:50:22 +0200

    [REWORK] rework of the api, to make easier to manipulate

--- detectionQCM/questiontype/api/correctapi.py
+++ detectionQCM/questiontype/api/correctapi.py
@@ -1,74 +1,46 @@
+import json
 import os
 import aiofiles
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from grip import export
 from pydantic import BaseModel
+import paths
 
 # run : uvicorn correctapi:questionapp --reload
 
 questionapp = FastAPI()
-PREFIXE = 'upload/'
-
-# Fonctions de test pour vérifier que le contact avec l'API se fait bien
-@questionapp.get("/question-assistant/greetings")
-async def greet():
-    return{"message":"Hello and welcome on the MCQ assistant GET service"}
-
-@questionapp.post("/question-assistant/greetings")
-async def greet():
-    return{"message":"Hello and welcome on the MCQ assistant POST service "}
-
-#Fonctions génériques de dépot de fichier 
-@questionapp.post("/question-assistant/uploadimage/")
-async def depose_image(clientfile: UploadFile):
-    res = await enregistreFichier(clientfile,["image/png","image/jpg"])
-    return res
-
-@questionapp.post("/question-assistant/uploadpdf/")
-async def depose_pdf(clientfile: UploadFile):
-    res = await enregistreFichier(clientfile,['application/pdf'])
-    return res
-
-## Partie temporaire, normalement, il faudrait envoyer les zones du template directement croppées et en png. 
-## Pour l'instant on envoie le template (pdf) et les indications des zones qu'il faut découper
-class QuestionTemplate(BaseModel):
-    examId: int
-    examName: str
-    gradeType: str
-    id: int
-    numero: int
-    point: int
-    step: int
-    typeAlgoName: str
-    typeId: int
-    zoneId: int
-
-# TODO: Faire qqch avec ces questions => Les envoyer à opencvpython pour qu'il les découpe ?  (Modifier la constante Zone)
-questionsTemplate = []
-
-@questionapp.post("/question-assistant/questiontemplate")
-async def renseigne_template(quest:QuestionTemplate):
-    # Actualise la liste des questions du template en mettant à jour la question si elle existe déjà
-    indextab = [i for i in range(0,len(questionsTemplate)) if questionsTemplate[i].id==quest.id]
-    if len(indextab)>0:
-        index = indextab[0]
-        del questionsTemplate[index]
-    questionsTemplate.append(quest) 
-    return {'question_loadded':quest in questionsTemplate ,'template_loadded':os.path.exists(PREFIXE+'template-'+str(quest.examId)+'.pdf')}
 
 
+@questionapp.get('/status/exam/{examid}')
+async def statusexam(examid : int):
+    return {'examId':examid,'exists':examid in loadExamInfos()}
 
-async def enregistreFichier(fichierclient : UploadFile, formats_acceptes :list[str]):
-    if fichierclient.content_type in formats_acceptes:
-        chemin_fichier = PREFIXE+fichierclient.filename
-        async with aiofiles.open(chemin_fichier, 'wb') as fichierlocal:
-            content = await fichierclient.read() 
-            await fichierlocal.write(content)  
-        return {"save": "success"}
-    else : return{"save":"fail","expected":formats_acceptes}
+@questionapp.get('/status/exam/{examid}/question/{questnum}')
+async def statusquestion(examid : int,questnum : int):
+    examinfos = loadExamInfos()
+    if(str(examid) not in examinfos):
+        return {'status':'error','reason':'exam_unknown'}
+    return {'status':'success','examId':examid,'questnum':questnum,'exists':questnum in examinfos[str(examid)]['question-numbers']}
+
+@questionapp.get('/')
+async def root():
+    return {'message':'connected to CorrectAPI'}
 
 
+# SAVING MANAGEMENT PART
+
+def loadExamInfos()->dict:
+    with open(paths.EXAMINFOS) as json_file:
+        examinfos = json.load(json_file)
+        return examinfos
+
+def saveExamInfos(examinfos :dict):
+    with open(paths.EXAMINFOS, 'w') as outfile:
+        examstr = str(json.dumps(examinfos,indent=2))
+        outfile.write(examstr)
+
+        
 # GESTION DES ERREURS CORS
 origins = [
     "http://localhost.tiangolo.com",
---
+++ detectionQCM/questiontype/api/paths.py
@@ -0,0 +1,4 @@
+#PATHS for the several files that will be saved (make sure they already exist)
+
+EXAMPATH = "../opencvpython/resource/" #(insert exam name just after)
+EXAMINFOS = "examinfos.json"
